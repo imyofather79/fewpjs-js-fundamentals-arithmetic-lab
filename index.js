ca5f27c7b9d474b85b130ffae96e44963918016a
imyofather79 <chunsum708@gmail.com>
2021-05-21 10:51:22 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+random;
+
+let num3 = 28;
+let num4 = 6;
+const mod = num3 % num4;
+
+
+const max = Math.max(1,20);
+max;
